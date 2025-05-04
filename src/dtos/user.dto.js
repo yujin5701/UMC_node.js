b8d781bde476dfafa0c5b6b8810082768e853e35
Yujin Gung <yoojin3117@ewhain.net>
2025-05-04 18:36:32 +0900

    Revert "Feature/chapter 05"

--- src/dtos/user.dto.js
+++ src/dtos/user.dto.js
@@ -1,5 +1,5 @@
 export const bodyToUser = (body) => {
-    const birth = new Date(body.birth + 'T00:00:00');
+    const birth = new Date(body.birth);
   
     return {
       email: body.email,
@@ -14,22 +14,17 @@ export const bodyToUser = (body) => {
   };
 
   export const responseFromUser = ({ user, preferences }) => {
-    const u = user[0]; // user는 배열이므로 첫 번째 값만 추출
-  
     return {
-      id: u.id,
-      email: u.email,
-      name: u.name,
-      gender: u.gender,
-      birth: u.birth.toISOString().split('T')[0], // 날짜 포맷 YYYY-MM-DD
-      address: u.address,
-      detailAddress: u.detail_address,
-      phoneNumber: u.phone_number,
-      preferences: preferences.map((pref) => ({
-        id: pref.id,
-        foodCategoryId: pref.food_category_id,
-        userId: pref.user_id,
-        name: pref.name,
-      })),
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        gender: user.gender,
+        birth: user.birth,
+        address: user.address,
+        detailAddress: user.detail_address,
+        phoneNumber: user.phone_number,     
+      },
+      preferences: preferences.map((p) => p.preference),
     };
   };
